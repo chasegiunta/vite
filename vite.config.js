dee2e5796f3140269654a2365dc8406ff556e633
Chase Giunta <me@chasegiunta.com>
2021-04-15 18:15:27 +0000

    sandbox fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  port: 3000,
+  server: {
+    //   host: 'https://3000-amber-vole-h19agbhp.ws-us03.gitpod.io',
+      cors: false,
+      hmr: {
+        port: 443,
+    },
+  }
 })
